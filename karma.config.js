9a405e7e3c22402988f5c02270edec337a64ef51
lihanzhou <lihanzhou@tencent.com>
2021-09-28 20:05:11 +0800

    配置chai

--- karma.config.js
+++ karma.config.js
@@ -10,7 +10,7 @@ module.exports = function (config) {
 
     // frameworks to use
     // available frameworks: https://www.npmjs.com/search?q=keywords:karma-adapter
-    frameworks: ['mocha'],
+    frameworks: ['mocha', 'chai'],
 
 
     // list of files / patterns to load in the browser
